bd8634da06500a67a20727d9e666162e832913e3
Hugo Clavijo <hugoused@hotmail.com>
2019-07-28 20:05:00 -0500

    Quit Game
    Close Button main menu

---
+++ Patatips/Assets/Close.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Close : MonoBehaviour
+{
+    private Button close;
+    // Start is called before the first frame update
+    void Start()
+    {
+        close.GetComponent<Button>();
+        close.onClick.AddListener(QuitGame);
+    }
+
+    private void QuitGame()
+    {
+
+        Application.Quit();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
