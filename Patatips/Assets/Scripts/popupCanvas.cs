c80ab741e6a579da3a3762e487690d0a36caac76
Hugo Clavijo <hugoused@hotmail.com>
2019-08-06 04:36:50 -0500

    Cat Game
    Board scene of the cat

--- Patatips/Assets/Scripts/popupCanvas.cs
+++ Patatips/Assets/Scripts/popupCanvas.cs
@@ -5,8 +5,9 @@ using UnityEngine.UI;
 
 public class popupCanvas : MonoBehaviour
 {
+     GameObject popup0;
      GameObject popup1;
-    GameObject popup2;
+     GameObject popup2;
      GameObject popup3;
      GameObject popup4;
     //private static GameObject popup;
@@ -14,6 +15,7 @@ public class popupCanvas : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        popup0 = GameObject.Find("PanelInst");
         popup1 = GameObject.Find("PanelBath");
         popup2 = GameObject.Find("PanelFood");
         popup3 = GameObject.Find("PanelBrush");
@@ -24,7 +26,7 @@ public class popupCanvas : MonoBehaviour
     {
         //popup = GameObject.Find(names);
         //popup.SetActive(false);
-
+        //popup0.SetActive(false);
         popup1.SetActive(false);
         popup2.SetActive(false);
         popup3.SetActive(false);
@@ -32,6 +34,15 @@ public class popupCanvas : MonoBehaviour
 
     }
 
+    public void OpenPop()
+    {
+      popup0.SetActive(true);
+    }
+
+    public void ClosePop()
+    {
+        popup0.SetActive(false);
+    }
 
     // Update is called once per frame
     void Update()
